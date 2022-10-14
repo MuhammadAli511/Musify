97297bcae4940fbc7987f2ccaf46977cb5c3394b
MuhammadAli511 <mohammadali417@outlook.com>
2022-10-14 23:40:14 +0500

    Sign Up Completed

---
+++ app/src/main/java/com/ass2/i190417_i192048/Signin.java
@@ -0,0 +1,14 @@
+package com.ass2.i190417_i192048;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
--- app/src/main/java/com/ass2/i190417_i192048/Signup.java
+++ app/src/main/java/com/ass2/i190417_i192048/Signup.java
@@ -1,14 +1,105 @@
 package com.ass2.i190417_i192048;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class Signup extends AppCompatActivity {
+    EditText name, email, password;
+    RadioGroup genderRadioGroup;
+    RadioButton genderRadioButton;
+    Button signup;
+    CheckBox terms;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        genderRadioGroup = findViewById(R.id.genderRadioGroup);
+        signup = findViewById(R.id.signup);
+        terms = findViewById(R.id.checkbox);
+        mAuth = FirebaseAuth.getInstance();
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!terms.isChecked()){
+                    Toast.makeText(Signup.this, "Please accept terms and conditions", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    int selectedId = genderRadioGroup.getCheckedRadioButtonId();
+                    genderRadioButton = findViewById(selectedId);
+                    String gender = genderRadioButton.getText().toString();
+                    if (name.getText().toString().equals("") || email.getText().toString().equals("") || password.getText().toString().equals("")){
+                        Toast.makeText(Signup.this, "Please fill all the fields", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                                .setDisplayName(name.getText().toString())
+                                                .build();
+                                        user.updateProfile(profileUpdates)
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            Toast.makeText(Signup.this, "User created successfully", Toast.LENGTH_LONG).show();
+                                                            Intent intent = new Intent(Signup.this, Signin.class);
+                                                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                            startActivity(intent);
+                                                        }
+                                                    }
+                                                });
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(Signup.this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                    }
+                }
+
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mAuth.getCurrentUser() != null){
+            Intent intent = new Intent(Signup.this, Signin.class);
+            startActivity(intent);
+        }
     }
 }

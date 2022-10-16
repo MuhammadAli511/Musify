ff62063d5e65e9e18a9494041147bc2571686a35
MuhammadAli511 <mohammadali417@outlook.com>
2022-10-16 18:25:31 +0500

    Test 2

--- app/src/main/java/com/ass2/i190417_i192048/MainScreen.java
+++ app/src/main/java/com/ass2/i190417_i192048/MainScreen.java
@@ -1,14 +1,35 @@
 package com.ass2.i190417_i192048;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainScreen extends AppCompatActivity {
     LinearLayout liked, add, search, listenlater;
+    RecyclerView playlistRecyclerViewMain;
+    FirebaseFirestore db;
+    List<Playlists> playlistsList;
+    PlaylistAdapter playlistAdapter;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +40,17 @@ public class MainScreen extends AppCompatActivity {
         add = findViewById(R.id.add);
         search = findViewById(R.id.search);
         listenlater = findViewById(R.id.listenlater);
+        playlistRecyclerViewMain = findViewById(R.id.playlistRecyclerViewMain);
+        playlistsList = new ArrayList<>();
+        db = FirebaseFirestore.getInstance();
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        playlistRecyclerViewMain.setLayoutManager(layoutManager);
+
+        getData();
+
+
+
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +60,28 @@ public class MainScreen extends AppCompatActivity {
         });
 
     }
+
+    public void getData(){
+        db.collection("playlists").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Playlists playlists = new Playlists(document.getString("playlistName"), document.getString("imageURL"));
+                        playlistsList.add(playlists);
+                    }
+                    playlistAdapter = new PlaylistAdapter(MainScreen.this, playlistsList);
+                    playlistRecyclerViewMain.setAdapter(playlistAdapter);
+                    playlistAdapter.notifyDataSetChanged();
+                } else {
+                    Toast.makeText(MainScreen.this, "Error getting documents.", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+
 }
 
